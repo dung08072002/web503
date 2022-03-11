9a4715aba8cac25d2f574ab444435aa452c80bcc
dung08072002 <dungnvph17931@fpt.edu.vn>
2022-03-11 07:14:36 +0700

    B2 Nodejs

--- src/app.js
+++ src/app.js
@@ -1,32 +1,16 @@
-const http = require('http');
+// const express = require("express");
+import express from 'express';
+import productRouter from './routes/product';
 
-const server = http.createServer((req, res) => {
-    console.log('url', req.url);
-    const url = req.url;
-    if(url === '/'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<body>');
-        res.write('<ul><li>User 1</li></ul>');
-        res.write('<ul><li>User 2</li></ul>');
-        res.write('<ul><li>User 3</li></ul>');
-        res.write('<ul><li>User 4</li></ul>');
-        res.write(` <form action="http://localhost:3000/create-user" method="POST">
-                        <input type="text" placeholder="username" id="username" name="username">
-                        <button id="btn-submit">Submit</button>
-                    </form>`);
-        res.write('</body>');
-        res.write('</html>');
-        res.end();
-    }else if (url === '/users'){
-        res.end();
-    }else if (url === '/create-user'){
-        res.end();
-    }
-})
+const app = express();
+// middleware
+app.use(express.json());
 
-const PORT = 3000;
+// Routing
+app.use("/api", productRouter);
 
-server.listen(PORT, () => {
-    console.log(`Server running port ${PORT}`);
-})
+// Connect
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server đang chạy cổng ${PORT}`);
+});
